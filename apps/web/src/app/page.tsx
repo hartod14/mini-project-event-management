96a15305f8e67ba3fbd3781cb92db75cde1137ca
hartod14 <haritody@gmail.com>
2025-01-22 16:37:27 +0700

    first init

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className="text-3xl text-red-500 font-bold underline">
+      Hello world!
+    </div>
+  )
+}
