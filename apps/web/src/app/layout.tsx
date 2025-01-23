5e7b7cd01b699fe2560d50240872d0ae3f409507
hartod14 <haritody@gmail.com>
2025-01-23 09:28:58 +0700

    build: setup web

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -13,12 +13,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         {children}
         <Footer />
