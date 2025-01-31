f75dc5e50c9b5afaaef65fd0093102426ac106ea
LAPTOP-KAAFL62K\User <haritody@gmail.com>
2025-01-31 19:55:24 +0700

    feat: init panel

---
+++ apps/web/src/app/panel/layout.tsx
@@ -0,0 +1,11 @@
+export default function PanelLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
---
+++ apps/web/src/app/panel/page.tsx
@@ -0,0 +1,7 @@
+export default function Panelpage() {
+    return (
+        <>
+            halowfsdafdas
+        </>
+    )
+}
